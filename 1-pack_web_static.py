8d1bcc9f9ef0b12004dfb02f0cd2445d76f0870d
francis waihiga <francisgwaihiga@gmail.com>
2024-01-11 23:07:15 +0000

    Task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+A fabric script that generates a .tgz archive fromm contents
+of the web static
+"""
+from fabric.api import local, task
+from datetime import datetime
+from pathlib import Path
+
+@task
+def do_pack():
+    """
+    fabric function to generate the .tgz
+    Returns:
+        Archive path if successful
+    """
+    # check if version folder exists and create if not existing
+    if not Path("versions").exists():
+        Path("versions").mkdir()
+
+    # Generate archive name
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    archivepath = f"versions/web_static_{time}.tgz"
+    # compress web static folder
+    results = local(f"tar -cvzf {archivepath} web_static")
+    # check if successful and return the path of the archive
+    if output.succeeded:
+        print(f"Archive created: {archivepath}")
+        return archivepath
+    else:
+        print("Failed to create the archive.")
+        return None
