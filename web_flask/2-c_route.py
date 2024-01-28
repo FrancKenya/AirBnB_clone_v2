1e73ce941a8ce07663670bc0077505bd0ef00bc1
Francis Waihiga <francisgwaihiga@gmail.com>
2024-01-28 22:31:24 +0000

    Task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
-
-"""
-Starts a flask web application
-The web applicatin is listening on 0.0.0.0, port 5000
-Uses the option strict_slashes=False in route definition
-"""
+""" Starts a web application with certain routes"""
 
 
 from flask import Flask
 app = Flask(__name__)
+
+
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """ displays hello hbnb string """
-    return 'Hello HBNB!'
+def hello_bnb():
+    """ Route displaying the string Hello HBNB """
+
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
-    """ displays HBNB """
-    return 'HBNB'
+def show_hbnb():
+    """ Route displaying HBNB string """
+
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route("/c/<text>", strict_slashes=False)
 def show_c(text):
-    """ dispays "C" followed by the value of the text variable """
+    """ Displays C followed by the value of the text variable """
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
 
