ecd0a499b9677ec587e953155c1e6c6839211e2d
Francis Waihiga <francisgwaihiga@gmail.com>
2024-01-28 15:09:11 +0000

    Task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""
+Starts a flask web application
+The web applicatin is listening on 0.0.0.0, port 5000
+Routes:
+    /: display “Hello HBNB!”
+    /hbnb: display “HBNB”
+    /c/<text>: display “C ” followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+Uses the option strict_slashes=False in route definition
+"""
+
+
+from flask import Flask
+app = Flask(__name__)
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ displays hello hbnb string """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """ displays HBNB """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def show_c(text):
+    """ dispays "C" followed by the value of the text variable """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
