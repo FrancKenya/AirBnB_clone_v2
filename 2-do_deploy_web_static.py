c7c8787c449810a889e0480568e83860bbcf7bda
francis waihiga <francisgwaihiga@gmail.com>
2024-05-12 06:56:48 -0700

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,6 +3,7 @@
 
 from fabric.api import *
 from pathlib import Path
+import os
 
 env.hosts = ['54.237.97.75', '100.25.144.98']
 
@@ -17,7 +18,7 @@ def do_deploy(archive_path):
         True if successful, false if otherwise
     """
     path_obj = Path(archive_path)
-    if not path_obj.exists():
+    if not path_obj.exists:
         return False
 
     tmpDest = f"/tmp/{path_obj.name}"
@@ -26,16 +27,15 @@ def do_deploy(archive_path):
     put(archive_path, tmpDest)
 
     # Execute deployment steps
-    exec_1 = run(f"mkdir -p {destDir}")
-    exec_2 = run(f"tar -xzf {tmpDest} -C {destDir}")
-    exec_3 = run(f"rm {tmpDest}")
-    exec_4 = run(f"mv {destDir}/web_static/* {destDir}")
-    exec_5 = run(f"rm -rf {destDir}/web_static")
-    exec_6 = run(f"rm -rf /data/web_static/current")
-    exec_7 = run(f"ln -s {destDir} /data/web_static/current")
+    exec_1 = sudo(f"mkdir -p {destDir}")
+    exec_2 = sudo(f"tar -xzf {tmpDest} -C {destDir}")
+    exec_3 = sudo(f"rm {tmpDest}")
+    exec_4 = sudo(f"mv {destDir}/web_static/* {destDir}")
+    exec_5 = sudo(f"rm -rf {destDir}/web_static")
+    exec_6 = sudo(f"rm -rf /data/web_static/current")
+    exec_7 = sudo(f"ln -s {destDir} /data/web_static/current")
     exec_list = [exec_1, exec_2, exec_3, exec_4, exec_5, exec_6, exec_7]
     if all([obj.succeeded for obj in exec_list]):
-        print("New version deployed!")
         return True
     else:
         return False
