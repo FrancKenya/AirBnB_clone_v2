1269c38fe096b64be3f09402f33da99d1e51cd98
francis waihiga <francisgwaihiga@gmail.com>
2024-05-16 22:00:14 +0300

    Task 5

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,23 @@
+# Using puppet to configure two web servers
+
+html_file = "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>"
+
+package { 'nginx':
+  ensure => installed,
+}
+exec {"cnfigure nginx":
+    command => 'sudo mkdir -p /data/web_static/releases/test/ &&sudo mkdir -p /data/web_static/shared/ &&
+                sudo echo $html_file > /data/web_static/releases/test/index.html &&
+                sudo ln -sf /data/web_static/releases/test/ /data/web_static/current &&
+                sudo chown -R ubuntu:ubuntu /data/ &&
+                new_update="\\\n\tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\t}" &&
+                sudo sed -i "55i $new_update" /etc/nginx/sites-available/default &&
+                sudo service nginx restart',
+    provider => 'shell',
+}
