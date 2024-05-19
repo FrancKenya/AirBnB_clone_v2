d463be8f4d24e04853fccaa7351501d24cb9c5ff
Francis Waihiga <francisgwaihiga@gmail.com>
2024-05-19 23:30:11 +0300

    Task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-A python script that starts Flask and makes a web application listen on
-on 0.0.0.0, port 5000
+This script starts Flask and makes a web application listen on
+on 0.0.0.0:5000
 """
 from flask import Flask
 app = Flask(__name__)
@@ -9,9 +9,9 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_bnb():
-    """ The Route displaying a string """
+    """ The Route displaying Hello HBNB! """
     return 'Hello HBNB!'
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
