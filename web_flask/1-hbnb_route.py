d9a8e07edf63c603b4b5553f491c3ee47d3a6a34
Francis Waihiga <francisgwaihiga@gmail.com>
2024-05-19 23:57:59 +0300

    Task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,15 +7,15 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_bnb():
-    """ Route displaying the string Hello HBNB """
+def hello_hbnb():
+    """ Fuction matched to the route that displays 'Hello HBNB!' """
 
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
-def show_hbnb():
-    """ Route displaying HBNB string """
+def display_hbnb():
+    """ Function matched to  route '/hbnb' and displays 'HBNB' """
 
     return "HBNB"
 
